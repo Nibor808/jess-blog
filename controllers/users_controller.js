231df53e26a89518b7fcacc07e3b49abc91af3db
robin <re@incubo.ca>
2017-03-03 10:17:59 -0500

    user controller and routes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -35,47 +35,60 @@ module.exports = UsersController = {
       });
   },
 
-  hashPassword(password) {
-    return new Promise((reject, resolve) => {
-      bcrypt.hash(password, null, null, (err, hash) => {
-        if(hash) {
-          console.log('hash',hash)
-          resolve(hash);
-        }else {
-          console.log('hashErr',err)
-          reject(err);
-        }
-      })
-    });
-  },
-
+  // save a new user
   saveUser(req, res) {
     if(!req.body.username || !req.body.email || !req.body.password) {
       res.status(422).send({ error: 'Please fill out all fields.' })
       return;
     }
 
-    // if(req.body.password !== req.body.passConfirm) {
-    //   res.status(422).send({ error: 'Your passwords must match.' });
-    //   return;
-    // }
-    // const password = module.exports.hashPassword(req.body.password);
+    if(req.body.password !== req.body.passConfirm) {
+      res.status(422).send({ error: 'Your passwords must match.' });
+      return;
+    }
 
-    knex('Users').insert({
-      email: req.body.email,
-      password: req.body.password,
-      username: req.body.username
-    })
-    .then(data => {
-      if(!data[0] > 0) {
-        res.status(422).send({ error: 'User was not saved.' });
-        return;
-      }else {
-        res.send({ ok: 'User saved.' });
-      }
-    })
-    .catch(err => {
-      res.status(422).send({ error: 'email or Username already in use.' });
-    })
+    bcrypt.hash(req.body.password, null, null, (err, hash) => {
+        if(err) {
+          console.log('hashErr',err)
+          return;
+        }else {
+          req.body.password = hash;
+
+          knex('Users').insert({
+            email: req.body.email,
+            password: req.body.password,
+            username: req.body.username
+          })
+          .then(data => {
+            if(!data[0] > 0) {
+              res.status(422).send({ error: 'User was not saved.' });
+              return;
+            }else {
+              res.send({ ok: 'User saved.' });
+            }
+          })
+          .catch(err => {
+            res.status(422).send({ error: 'Email or Username already in use.' });
+          })
+        }
+      })
+  },
+
+  // delete user
+  deleteUser(req, res) {
+    //first delete any comments associated with the user
+    knex('Comments').where('user_id', req.params.id).del();
+
+    knex('Users').where('id', req.params.id).del()
+      .then(data => {
+        if(!data == 1) {
+          res.status(422).send({ error: 'User doesn\'t exist.' });
+        }else {
+          res.send({ ok: 'User deleted.' });
+        }
+      })
+      .catch(err => {
+        res.status(422).send({ error: 'User not deleted.' });
+      });
   }
 };
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -4,4 +4,5 @@ module.exports = function(app) {
   app.get('/users', UserController.getUsers);
   app.get('/user/:id', UserController.getUser);
   app.post('/saveuser', UserController.saveUser);
+  app.post('/deleteuser/:id', UserController.deleteUser);
 };
