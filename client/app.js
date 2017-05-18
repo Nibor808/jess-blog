3008f304555ce323222d30126f9e3aa11a4f3ee9
robin <re@incubo.ca>
2017-05-17 23:44:25 -0400

    removed app.js from .gitignore

---
+++ client/app.js
@@ -0,0 +1,14 @@
+// only used to copy to dist
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res) {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening')
+});
